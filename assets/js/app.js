ac4b37aab023eff33bda6fbedcf582e2f1bb473e
NicolaLampis <nico.lampo@gmail.com>
2021-01-27 19:07:13 +0000

    solved all bug on game functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,90 @@
 let cards = ["apple", "apple", "banana", "banana", "orange", "orange", "ananas", "ananas", "strawberry", "strawberry", "cherry", "cherry"];
-const card_deck = document.querySelectorAll(".card-box-inner");
+let deckCard = document.querySelectorAll(".card-box-inner");
 let alreadyFlipped = false;
 let firstCard, secondCard;
+let matchWin = 0;
+let stopRotation = false;
 
-// target and flip the card when clicked
+
+
+var localUser = localStorage.getItem($("#username").val());
+//  Modal
+if (typeof(Storage) !== "undefined") {
+// Retrieve
+  document.getElementById("username").innerHTML = localStorage.getItem("localUser");
+};
+//  --
+$("#saveName").on("click", function() {
+  $("#user_name").text( $("#username").val());
+});
+//  --
+localStorage.setItem("localUser", $("#username").val());
+
+
+
+// Wait-Listen for click
+deckCard.forEach(currentValue => currentValue.addEventListener("click", flipCard));
+// flip the card only after these conditions
 function flipCard() {
+    if (stopRotation) return;
+    if (this === firstCard) return;
     this.classList.add("flip");
 // first click    
-    if (!alreadyFlipped) {
+    if (!alreadyFlipped) {        
         alreadyFlipped = true;
         firstCard = this;
-// second click        
-    } else {
-        alreadyFlipped = false;
+        console.log(firstCard);
+        return;
+    };
+// second click     
+        
         secondCard = this;
-        ceckMatch();
-    };
-}
-
-        function ceckMatch() {
-        if (firstCard.dataset.card === secondCard.dataset.card) {
-            firstCard.removeEventListener("click", flipCard);
-            secondCard.removeEventListener("click", flipCard);
-            console.log("match")
-        } else {
-            setTimeout(function() {
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip");
-            console.log("not a match");
-        }, 800);  // timeout helps to see the rotation of the second card
-        }
+        console.log(secondCard);
+        ceckMatch();    
     };
 
-  
+// The cards are not clickable, both first and second card
+function disableClick() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
 
+    stopRotation = false;
+    alreadyFlipped = false;
+    firstCard = null;
+    secondCard = null;
+}    
 
 // check if there is a match between two cards
+        function ceckMatch() {
+        if (firstCard.dataset.card === secondCard.dataset.card) {
+            matchWin++;
+            console.log(matchWin);
+            disableClick();
+        } else {
+            stopFlippinCard();
+        }
+        
+        youWin();
+    };
+function stopFlippinCard() {
+    stopRotation = true; 
+
+    setTimeout(function() {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        stopRotation = false;
+        alreadyFlipped = false;
+        firstCard = null;
+        secondCard = null;
+    }, 1400);
+}
+  
+// timeout helps to see the rotation of the second card
 
 
-card_deck.forEach(currentValue => currentValue.addEventListener("click", flipCard));
+
+
+
 
 // Shuffle all 12 cards
 // Fisher-Yates shuffle algorithm logic
@@ -56,11 +102,20 @@ function shuffle(cards) {
 shuffle(cards);
 console.log(cards);
 
+// after the shuffle, re-assign the cards data attribute to the new cards deck
 function assignCardDeck() {
     $(".card-box-inner").each(function(index) {
         $(this).attr("data-card", cards[index]);
     });
-
 }
 
-
+//  XXX toast           XXX  XXX  XXX
+function youWin() {
+    if (matchWin == 6) {setTimeout(function() {
+        console.log("wiiinnnnnnnnn");
+        alert("YOU WIN");}, 800);      
+    };
+};
+function resetGame() {
+    matchWin = 0;
+}
